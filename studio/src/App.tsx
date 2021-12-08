925dc1d84e33be38c325185f2ffed4aadf45211a
nst-t <tyler@nstrumenta.com>
2021-12-08 10:08:16 -0800

    kGetModInfo

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -1,9 +1,22 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { NstrumentaClient } from "nstrumenta";
+import { Button, Typography } from "@mui/material";
 
 function App() {
+  const [traxState, setTraxState] = useState<string>();
+  const [modInfo, setModInfo] = useState<string>();
+
+  const nstRef = useRef<NstrumentaClient>();
+
+  const getModInfo = () => {
+    if (!nstRef.current) return;
+    // kGetModInfo
+    const kGetModInfoCommand = new Uint8Array([0x00, 0x05, 0x01, 0xef, 0xd4]);
+    console.log("sending kGetModInfo", kGetModInfoCommand);
+    nstRef.current.sendBuffer("trax-in", kGetModInfoCommand);
+  };
+
   useEffect(() => {
     //get wsUrl from params if present
     const wsUrlParam = new URLSearchParams(window.location.search).get("wsUrl");
@@ -16,6 +29,8 @@ function App() {
       wsUrl,
     });
 
+    nstRef.current = nstClient;
+
     console.log("connecting to ", wsUrl);
 
     nstClient.addListener("open", () => {
@@ -24,44 +39,40 @@ function App() {
         console.log("serialport-events", message);
         switch (message.type) {
           case "open":
-            // kGetModInfo
-            const kGetModInfoCommand = new Uint8Array([
-              0x00, 0x05, 0x01, 0xef, 0xd4,
-            ]);
-            console.log("sending kGetModInfo", kGetModInfoCommand);
-            nstClient.sendBuffer("trax-in", kGetModInfoCommand);
+            setTraxState("open");
+            getModInfo();
         }
       });
       nstClient.subscribe("trax2", (message) => {
+        if (traxState !== "open") setTraxState("open");
         console.log("trax2", message);
+        const frameId = message.data[2];
+        switch (frameId) {
+          case 0x02:
+            const modInfo = new TextDecoder().decode(
+              new Uint8Array(message.data.slice(3, -2))
+            );
+            console.log(JSON.stringify(message), modInfo);
+            setModInfo(modInfo);
+            break;
+          default:
+            console.log(`unhandled frame ID: ${frameId}`, message);
+            break;
+        }
       });
-
-    
-      // // kGetModInfo
-      // const kGetModInfoCommand = new Uint8Array([0x00, 0x05, 0x01, 0xef, 0xd4]);
-      // console.log("sending kGetModInfo", kGetModInfoCommand);
-      // nstClient.sendBuffer("trax", kGetModInfoCommand);
-
     });
     nstClient.init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography>
+        <Button variant="contained" onClick={getModInfo}>
+          kGetModInfo
+        </Button>
+        {modInfo}
+      </Typography>
     </div>
   );
 }
